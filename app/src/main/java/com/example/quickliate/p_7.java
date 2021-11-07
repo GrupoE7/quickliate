4ade7308e478a3e1f8f020139e5916974daa9e64
zuany <zuany1706@gmail.com>
2021-11-07 04:12:53 -0500

    activity_p7 activity_p13

--- app/src/main/java/com/example/quickliate/p_7.java
+++ app/src/main/java/com/example/quickliate/p_7.java
@@ -29,23 +29,17 @@ public class p_7 extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_p7);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_p7);
+        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_p7);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+   // @Override
+   // public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_p7);
+      //  return NavigationUI.navigateUp(navController, appBarConfiguration)
+      //          || super.onSupportNavigateUp();
+   // }
 }
