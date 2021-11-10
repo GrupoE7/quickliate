ad9f6214558d841cee3d40596d0486db2e384cf8
Laura Pacheco <laurayesenia19@hotmail.com>
2021-11-10 08:49:27 -0500

    Actualización pantalla de bienvenida, agregado de pantalla de login con enlace a perfil de administrador y  recuperar contraseña

--- app/src/main/java/com/example/quickliate/MainActivity.java
+++ app/src/main/java/com/example/quickliate/MainActivity.java
@@ -3,23 +3,52 @@ package com.example.quickliate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Constante para modificar el tiempo destinado para la bienvenida
+    private static final long SPLASH_SCREEN_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Bloquear la rotación de pantalla, quedará vertical obligatoriamente
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //Ocultar la barra de notificaciones
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        //Ocultar ActionBar
+        getSupportActionBar().hide();
+
+        //Crear tarea de temporización
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //Llamar a la siguiente actividad
+                Intent newIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                //Lanzar la actividad
+                startActivity(newIntent);
+                //Cerrar splash screen
+                finish();
+            }
+        };
+
+        //Lanzar el temporizador
+        Timer timer = new Timer();
+        timer.schedule(timerTask, SPLASH_SCREEN_DELAY);
+
         setContentView(R.layout.activity_main);
     }
-    //methodo
-    public void siguiente(View view){
-        Intent i = new Intent(this,p_5.class);
-        startActivity(i);
-
-    }
-
-
-
 }
