5746f938e46f7f632bd2882628ee686aea3d245f
acalderonmor <86635497+acalderonmor@users.noreply.github.com>
2021-12-05 21:20:26 -0500

    gps

--- app/src/main/java/com/example/quickliate/Activity7.java
+++ app/src/main/java/com/example/quickliate/Activity7.java
@@ -1,12 +1,9 @@
 package com.example.quickliate;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 
 public class Activity7 extends AppCompatActivity {
@@ -27,7 +24,7 @@ public class Activity7 extends AppCompatActivity {
 
     }
     public void alarma(View view){
-        Intent b = new Intent(this,AlertasActivity.class);
+        Intent b = new Intent(this,p_5.class);
         startActivity(b);
 
     }
@@ -37,7 +34,7 @@ public class Activity7 extends AppCompatActivity {
 
     }
     public void irubica(View view){
-        Intent d = new Intent(this,Activity12.class);
+        Intent d = new Intent(this,MapsActivity.class);
         startActivity(d);
 
     }
@@ -45,32 +42,5 @@ public class Activity7 extends AppCompatActivity {
         Intent e = new Intent(this,activity_4.class);
         startActivity(e);
 
-    }
-
-    //Metodo para confirmar si el usuario quiere realmente cerrar sesión
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-        //Averiguar si el evento es pulsar boton de devolverse
-        if(keyCode == KeyEvent.KEYCODE_BACK) {
-            //Crear en tiempo de ejecución, cuadro de dialogo
-            new AlertDialog.Builder(this, R.style.Theme_AppCompat_Dialog_Alert)
-                    //Icono que se va a mostrar, icono siguiente
-                    .setIcon(android.R.drawable.ic_dialog_info)
-                    //Titulo que debe poner al dialogo
-                    .setTitle("Información")
-                    //Mensaje que debe poner al cuadro de dialogo
-                    .setMessage("¿Desea cerrar sesión?")
-                    //Boton negativo, parametros: que dira el boton, y que hace al seleccionar
-                    .setNegativeButton("No", null)
-                    //Boton positivo, new DialogInter... es un metodo escuchador
-                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //Cerrar la aplicacion
-                            Activity7.this.finish();
-                        }
-                    }).show();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
     }
 }
